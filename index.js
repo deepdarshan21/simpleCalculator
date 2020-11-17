60bc86f97db19425903be9fc7386d1ecca2022d4
Deepdarshan <deepdarshan21@gmail.com>
2020-11-18 02:58:25 +0530

    BODMAS implimented

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
-var ans=0;
-var output="";
-var plusminus="plus"
-function opt()
-{
-    document.getElementById("result").innerHTML="ANS";
+var ans = 0;
+var output = "";
+var plusminus = "plus"
+function opt() {
+    ans = eval(output);
+    document.getElementById("result").innerHTML = ans;
 }
-function input(opr)
-{
-    if(opr=="del")
-    {
-        output=output.slice(0,-1);
+function input(opr) {
+    if (opr == "del") {
+        output = output.slice(0, -1);
     }
-    else if(opr=="C")
-    {
-        output=0;
+    else if (opr == "C") {
+        output = 0;
     }
-    else if(opr=="+/-")
-    {
-        if(plusminus=="plus")
-        {
-            plusminus="minus";
-            output='-'+output;
+    else if (opr == "+/-") {
+        if (plusminus == "plus") {
+            plusminus = "minus";
+            output = '-' + output;
         }
-        else
-        {
-            plusminus="plus";
-            output=output.substring(1);
+        else {
+            plusminus = "plus";
+            output = output.substring(1);
         }
 
     }
-    else
-    {
-        output+=opr;
+    else {
+        output += opr;
     }
-    if(output=="")
-    {
-        output=0;
+    if (output == "") {
+        output = 0;
     }
-    document.getElementById("result").innerHTML=output;
-    if(opr=="C")
-    {
-        output="";
+    document.getElementById("result").innerHTML = output;
+    if (opr == "C") {
+        output = "";
     }
 }
 
